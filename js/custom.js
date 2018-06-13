441a58b7e0ee515c8a5e35560e7db5246250267b
hseniht <thinesh_kumar_mu@hotmail.com>
2018-06-13 10:28:42 +0800

    added some basic json script in js

--- js/custom.js
+++ js/custom.js
@@ -42,11 +42,60 @@ $(document).ready(function(){
       $("table").show();
       $("#hidetable").show();
       $("#showtable").hide();
+      $("#num-plate").hide();
     });
+
     $("#hidetable").click(function(){
-        $("#showtable").show();
-        $("#hidetable").hide();
-        $("table").hide();
+      $("num-plate").show();
+      $("#showtable").show();
+      $("#hidetable").hide();
+      $("table").hide();
+    });
+
+    $("#hidetable").click(function(){
+      $("num-plate").show();
+      $("#showtable").show();
+      $("#hidetable").hide();
+      $("table").hide();
     });
 
 });
+
+
+
+
+/*fetch data */
+
+var jsonObj = [{
+       "id": "73",
+       "visitor_id": "3",
+        "type": "navigation",
+        "key": "job_application",
+        "value": "\/job\/customer-service-representative-31\/apply\/",
+        "created_at": "2018-01-23 04:08:07"
+    },
+    {
+        "id": "74",
+        "visitor_id": "3",
+        "type": "job_application",
+        "key": "application_reference",
+        "value": "JO-1801-152151_1516600755",
+        "created_at": "2018-01-23 04:08:20"
+    },
+    {
+        "id": "76",
+        "visitor_id": "3",
+        "type": "filter",
+        "key": "job_types",
+        "value": "temporary",
+        "created_at": "2018-01-23 04:08:51"
+    }
+]
+
+
+// document.querySelector('h1').innerText = 'Type of jsonObj: '+ typeof jsonObj;
+
+/*jsonObj = JSON.stringify(jsonObj);  to json*/
+
+console.log(jsonObj);
+console.log(jsonObj[1].visitor_id);
